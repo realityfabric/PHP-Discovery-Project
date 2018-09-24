b5ae4fce9fb5210a0e970ab4cb2362ae5b30745d
realityfabric <realityfabric@gmail.com>
2018-09-23 18:55:50 -0700

    Add index.php
    Designed per Discovery Project exercise 2-1

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title></title>
+
+</head>
+<body>
+<p>[This is the Header Placeholder]</p>
+<p>[This is the Button Navigation Placeholder]</p>
+<p>[This is the Dynamic Content Placeholder]</p>
+<p>[This is the Footer Placeholder]</p>
+</body>
+</html>
