ced94d40557c116a6827a7bea6978776563e4b95
realityfabric <realityfabric@gmail.com>
2018-10-01 16:00:13 -0700

    Create inc_php_info.php
    Per discovery project exercise 3-2

---
+++ www/includes/inc_php_info.php
@@ -0,0 +1,6 @@
+<?php
+include("home_links_bar.inc");
+
+echo "<p><b>PHP</b> is used in Web Development as a server side scripting language that is both flexible and powerful.</p>";
+
+echo "<p>PHP allows the server to access files that would typically be hidden from the user, without allowing the user to see the code which is being run. It can access regular files, or even access databases.</p>"
