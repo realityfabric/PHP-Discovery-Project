22d2a3852446cbdf49a1043ea998a1fe92f97c37
realityfabric <realityfabric@gmail.com>
2018-10-01 15:59:45 -0700

    Create inc_chinese_zodiac.php
    Per discovery project exercise 3-1

---
+++ www/includes/inc_chinese_zodiac.php
@@ -0,0 +1,8 @@
+<?php
+include("home_links_bar.inc");
+
+echo "<p>The Chinese Zodiac is similar to the Zodiac associated with western astrology. The Chinese Zodiac assigns an animal to each of 12 one-year long periods. At the end of the 12 years the zodiac repeats itself.</p>";
+
+echo "<p>The animals are as follows: Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, and Pig.</p>";
+
+echo "<p>The Chinese Zodiac is similar in its complexity to the Western Zodiac. Where the Western Zodiac has Sun and Moon signs (among a number of others), the Chinese Zodiac has Inner, True, and Secret signs.</p>";
