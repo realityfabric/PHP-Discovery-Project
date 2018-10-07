656d6d8cb935e3b86d18eead0e968a0d661abbcd
realityfabric <realityfabric@gmail.com>
2018-10-07 10:17:59 -0700

    Add href to text links
    Per Discovery Project 4-3

--- www/includes/inc_text_links.php
+++ www/includes/inc_text_links.php
@@ -2,13 +2,13 @@
 <!-- I think this is what the textbook is talking about, but it does not elaborate. -->
 <div>
 <ul>
-<li><a>Home Page</a></li>
-<li><a>Site Layout</a></li>
-<li><a>Control Structures</a></li>
-<li><a>String Functions</a></li>
-<li><a>Web Forms</a></li>
-<li><a>Midterm Assessment</a></li>
-<li><a>State Information</a></li>
-<li><a>User Templates</a></li>
-<li><a>Final Project</a></li>
+<li><a href="index.php?page=home_page">Home Page</a></li>
+<li><a href="index.php?page=site_layout">Site Layout</a></li>
+<li><a href="index.php?page=control_structures">Control Structures</a></li>
+<li><a href="index.php?page=string_functions">String Functions</a></li>
+<li><a href="index.php?page=web_forms">Web Forms</a></li>
+<li><a href="index.php?page=midterm_assessment">Midterm Assessment</a></li>
+<li><a href="index.php?page=state_information">State Information</a></li>
+<li><a href="index.php?page=user_templates">User Templates</a></li>
+<li><a href="index.php?page=final_project">Final Project</a></li>
 </ul></div>
