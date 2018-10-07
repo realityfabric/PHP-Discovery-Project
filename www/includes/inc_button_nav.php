e598ccc159a7370ba3a77490b185c4e3a1c67ea6
realityfabric <realityfabric@gmail.com>
2018-10-07 09:28:26 -0700

    Add links to navigation buttons
    Per Discovery Project 4-2

--- www/includes/inc_button_nav.php
+++ www/includes/inc_button_nav.php
@@ -1,12 +1,30 @@
 <!-- Inserts the nine buttons created in Discovery Project 1-2 -->
 <div id="navbar">
-<img src="static/HomePage.png" />
-<img src="static/SiteLayout.png" />
-<img src="static/ControlStructures.png" />
-<img src="static/StringFunctions.png" />
-<img src="static/WebForms.png" />
-<img src="static/MidtermAssessment.png" />
-<img src="static/StateInformation.png" />
-<img src="static/UserTemplates.png" />
-<img src="static/FinalProject.png" />
+<a href="index.php?page=home_page">
+	<img class="btn" src="static/HomePage.png" alt="[Home Page]" title="Home Page" style="border:0" />
+</a>
+<a href="index.php?page=site_layout">
+	<img class="btn" src="static/SiteLayout.png" alt="[Site Layout]" title="Site Layout" style="border:0" />
+</a>
+<a href="index.php?page=control_structures">
+	<img class="btn" src="static/ControlStructures.png" alt="[Control Structures]" title="Control Structures" style="border:0" />
+</a>
+<a href="index.php?page=string_functions">
+	<img class="btn" src="static/StringFunctions.png" alt="[String Functions]" title="String Functions" style="border:0" />
+</a>
+<a href="index.php?page=web_forms">
+	<img class="btn" src="static/WebForms.png" alt="[Web Forms]" title="Web Forms" style="border:0" />
+</a>
+<a href="index.php?page=midterm_assessment">
+	<img class="btn" src="static/MidtermAssessment.png" alt="[Midterm Assessment]" title="Midterm Assessment" style="border:0" />
+</a>
+<a href="index.php?page=state_information">
+	<img class="btn" src="static/StateInformation.png" alt="[State Information]" title="State Information" style="border:0" />
+</a>
+<a href="index.php?page=user_templates">
+	<img class="btn" src="static/UserTemplates.png" alt="[User Templates]" title="User Templates" style="border:0" />
+</a>
+<a href="index.php?page=final_project">
+	<img class="btn" src="static/FinalProject.png" alt="[Final Project]" title="Final Project" style="border:0" />
+</a>
 </div>
