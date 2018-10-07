d4b0047025f9c9983b21c43072c4b6f3d57a381d
realityfabric <realityfabric@gmail.com>
2018-10-07 10:52:21 -0700

    Remove unnecessary PHP

--- www/includes/inc_home_links_bar.php
+++ www/includes/inc_home_links_bar.php
@@ -1,6 +1,4 @@
-<?php
-// header bar
-echo "<table><tr>";
-echo "<th><a>PHP</a></th>";
-echo "<th><a>Chinese Zodiac</a></th>"
-echo "</tr></table>";
+<table><tr>
+<th><a>PHP</a></th>
+<th><a>Chinese Zodiac</a></th>
+</tr></table>
