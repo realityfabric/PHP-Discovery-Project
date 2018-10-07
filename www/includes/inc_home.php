e9521508ee2e1cd363439ccddbb01e56bb357f47
realityfabric <realityfabric@gmail.com>
2018-10-07 10:57:57 -0700

    Add php to dynamically alter home page based on links
    Per Discovery Project 4-5

--- www/includes/inc_home.php
+++ www/includes/inc_home.php
@@ -1,1 +1,18 @@
-<p>[Insert home page content here]</p>
+<?php
+include("inc_home_links_bar.php");
+
+if (isset($_GET['section'])) {
+	switch ($_GET['section']) {
+		case 'zodiac':
+			include("includes/inc_chinese_zodiac.php");
+			break;
+		case 'php': // A value of 'php' means
+					// to display the default page
+		default:
+			include('includes/inc_php_info.php');
+			break;
+	}
+} else { // if no section has been selected, then display the
+		 // default page
+		 include('includes/inc_php_info.php');
+}
