fe1f41fa75d83023c6290c5fe23aafac35d42ce8
root <realityfabric@gmail.com>
2018-11-12 09:31:28 -0800

    Add primary key constraint to allow auto_increment

--- createRandomProverbsTable.sql
+++ createRandomProverbsTable.sql
@@ -1,7 +1,7 @@
 USE chinese_zodiac;
 
 CREATE TABLE randomproverb (
-	proverb_number INT AUTO_INCREMENT,
+	proverb_number INT AUTO_INCREMENT PRIMARY KEY,
     proverb VARCHAR(255), -- at least 100 characters
     display_count INT
 );
