257531e5100192e7e2f69cb02ff0b64bc73cf1a3
root <realityfabric@gmail.com>
2018-11-12 09:29:08 -0800

    Add auto_increment to id field

--- createRandomProverbsTable.sql
+++ createRandomProverbsTable.sql
@@ -1,7 +1,7 @@
 USE chinese_zodiac;
 
 CREATE TABLE randomproverb (
-	proverb_number INT,
+	proverb_number INT AUTO_INCREMENT,
     proverb VARCHAR(255), -- at least 100 characters
     display_count INT
 );
